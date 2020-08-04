854c5228fb59b0c34717d5a5b4b51c3ca25f0d7f
Aurora Stark <69068989+AuroraStark@users.noreply.github.com>
2020-08-04 20:36:05 +0530

    Update msp.py

--- msp.py
+++ msp.py
@@ -5,13 +5,13 @@ def minproduct(a,b,n,k):
         p = a[i]*b[i]
         res = res + p
         if (p<0 and b[i]<0):
-            temp = (a[i]+2*k)+b[i]
+            temp = (a[i]+2*k)*b[i]
         elif (p<0 and a[i]<0):
-            temp = (a[i]-2*k)+b[i]
+            temp = (a[i]-2*k)*b[i]
         elif (p>0 and a[i]<0):
-            temp = (a[i]+2*k)+b[i]
+            temp = (a[i]+2*k)*b[i]
         elif (p>0 and a[i]>0):
-            temp = (a[i]-2*k)+b[i]
+            temp = (a[i]-2*k)*b[i]
         d = abs(p-temp)
         if d>diff:
             diff = d
